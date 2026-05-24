581311f8db16adff89e743c732286928269573ab
hacumrecanta-hash <hacumrecanta@gmail.com>
2026-05-25 01:49:19 +0200

    bridge firebase to compat structure

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
-// Firebase Config & Auth
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, onSnapshot, query, orderBy } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+// 1. Tarayıcı ve Vercel dostu güvenli COMPAT CDN linkleri (eval içermez)
+import firebase from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js';
+import 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth-compat.js';
+import 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore-compat.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAJ-Fy5Qai4gAFBa55cGer8O3l8neMy2zI",
@@ -12,13 +12,17 @@ const firebaseConfig = {
   appId: "1:161310364502:web:47d52c38df6a3f60c6bcf7"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
+// Firebase'i güvenli yöntemle başlatıyoruz
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+const auth = firebase.auth();
 
 // Auth guard - sayfayı korur
 export function requireAuth(callback) {
-  onAuthStateChanged(auth, (user) => {
+  auth.onAuthStateChanged((user) => {
     if (user) {
       document.getElementById('loading-screen')?.remove();
       callback(user);
@@ -29,9 +33,47 @@ export function requireAuth(callback) {
 }
 
 export function logout() {
-  signOut(auth).then(() => {
+  auth.signOut().then(() => {
     window.location.href = '/index.html';
   });
 }
 
-export { db, auth, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, onSnapshot, query, orderBy, signInWithEmailAndPassword, onAuthStateChanged };
+// Diğer sayfaların (Vite/Webpack veya düz import modülleri) bozulmaması için eski fonksiyon isimlerini birebir dışa aktarıyoruz
+const collection = (database, path) => database.collection(path);
+const getDocs = (queryInstance) => queryInstance.get();
+const addDoc = (collectionInstance, data) => collectionInstance.add(data);
+const doc = (database, collPath, docId) => database.collection(collPath).doc(docId);
+const updateDoc = (docInstance, data) => docInstance.update(data);
+const deleteDoc = (docInstance) => docInstance.delete();
+
+// onSnapshot, query ve orderBy fonksiyonlarını v8 yapısına uygun köprü olarak kuruyoruz
+const query = (collectionInstance, ...constraints) => {
+  let q = collectionInstance;
+  constraints.forEach(constraint => {
+    if (constraint && constraint.type === 'orderBy') {
+      q = q.orderBy(constraint.field, constraint.direction);
+    }
+  });
+  return q;
+};
+const orderBy = (field, direction = 'asc') => ({ type: 'orderBy', field, direction });
+const onSnapshot = (queryInstance, callback) => queryInstance.onSnapshot(callback);
+
+const signInWithEmailAndPassword = (authInstance, email, password) => authInstance.signInWithEmailAndPassword(email, password);
+const onAuthStateChanged = (authInstance, callback) => authInstance.onAuthStateChanged(callback);
+
+export { 
+  db, 
+  auth, 
+  collection, 
+  addDoc, 
+  getDocs, 
+  doc, 
+  updateDoc, 
+  deleteDoc, 
+  onSnapshot, 
+  query, 
+  orderBy, 
+  signInWithEmailAndPassword, 
+  onAuthStateChanged 
+};
